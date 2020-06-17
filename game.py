edb00d9f820d92f12eb1bf4640174545e2adc299
Eszkis <laszlo.szkis@gmail.com>
2020-06-17 11:19:33 +0200

    added function to upgrade characters skills

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 my_map = [
     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', '11', 'E', 'E', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', '21', 'X'],
+    ['X', '11', 'E', 'C', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', '21', 'X'],
     ['X', 'E', 'X', 'E', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'E', 'X', 'E', 'X'],
-    ['X', 'E', 'E', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'X'],
+    ['X', 'S', 'E', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'E', 'E', 'X'],
     ['X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X'],
     ['X', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'X'],
     ['X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X', 'B', 'X'],
@@ -38,11 +38,39 @@ symbols = {
     'X': 'wall',
     'E': 'empty',
     'B': 'box',
+    'C': 'count_upgrade',
+    'S': 'size_upgrade',
 }
 
 
-def step_player(player_state, player_next):
+def step_player(player_state, player_next, character_list, character_name):
     print(player_state)
-    if my_map[player_next[0]][player_next[1]] == 'E':
-        my_map[player_next[0]][player_next[1]] = '1' + player_next[2]
+    tile = my_map[player_next[0]][player_next[1]]
+    valid_slots = ['E', 'C', 'S']
+    current_character = get_current_player(character_list, character_name)
+    if tile in valid_slots:
+        update_character(character_list, character_name, tile)
+        my_map[player_next[0]][player_next[1]] = current_character['user_id'] + player_next[2]
         my_map[player_state[0]][player_state[1]] = 'E'
+
+
+def create_character(nickname, user_id):  # id még kell dolgozni, hogy az is autogeenrált legyen 1-4 között
+    new_character = {'name': nickname, 'user_id': user_id, 'bomb_count': 1, 'bomb_size': 2, 'life': 1}
+    return new_character
+
+
+def update_character(character_list, character_name, upgrade_type):
+    current_character = get_current_player(character_list, character_name)
+    if upgrade_type == 'C':
+        current_character['bomb_count'] += 1
+    elif upgrade_type == 'S':
+        current_character['bomb_size'] += 1
+    return None
+
+
+def get_current_player(character_list, character_name):
+    current_character = {}
+    for character in character_list:
+        if character['name'] == character_name:
+            current_character = character
+    return current_character
--- static/js/dom.js
+++ static/js/dom.js
@@ -2,62 +2,64 @@ import {playerMove, refreshMap} from "./data_handler.js";
 
 
 export function dom() {
-    document.addEventListener('keydown', playerMovement);
-    refreshMap(showMap);
+  document.addEventListener('keydown', playerMovement);
+  refreshMap(showMap);
 }
 
 function playerMovement(e) {
-    let playerNode = document.querySelector('[data-player="1"]');
-    let playerRowIndex = parseInt(playerNode.dataset.row);
-    let playerColIndex = parseInt(playerNode.dataset.col);
-    let playerState = [playerRowIndex, playerColIndex];
-    let playerNext = [];
-    let acceptableInput = [37, 38, 39, 40];
-    if (acceptableInput.includes(e.keyCode)) {
-        switch (e.keyCode) {
-            case 37: // left
-                playerNext = [playerRowIndex, playerColIndex - 1, '1'];
-                break;
-            case 39: // right
-                playerNext = [playerRowIndex, playerColIndex + 1, '2'];
-                break;
-            case 38: // up
-                playerNext = [playerRowIndex - 1, playerColIndex, '3'];
-                break;
-            case 40: // down
-                playerNext = [playerRowIndex + 1, playerColIndex, '4'];
-                break;
-        }
-        playerMove(playerState, playerNext);
+  let playerNode = document.querySelector('[data-player="1"]');
+  let playerRowIndex = parseInt(playerNode.dataset.row);
+  let playerColIndex = parseInt(playerNode.dataset.col);
+  let playerState = [playerRowIndex, playerColIndex];
+  let playerNext = [];
+  let acceptableInput = [37, 38, 39, 40];
+  if (acceptableInput.includes(e.keyCode)) {
+    switch (e.keyCode) {
+      case 37: // left
+        playerNext = [playerRowIndex, playerColIndex - 1, '1'];
+        break;
+      case 39: // right
+        playerNext = [playerRowIndex, playerColIndex + 1, '2'];
+        break;
+      case 38: // up
+        playerNext = [playerRowIndex - 1, playerColIndex, '3'];
+        break;
+      case 40: // down
+        playerNext = [playerRowIndex + 1, playerColIndex, '4'];
+        break;
     }
+    playerMove(playerState, playerNext);
+  }
 }
 
 function showMap(data) {
-    let map = document.querySelector(".map");
-    map.innerHTML = '';
-    let row_index = 0;
-    for (let row of data) {
-        let col_index = 0;
-        let divRow = '';
-        for (let cell of row) {
-            if (cell.length != 2) {
-                divRow += `<div class="cell ${symbols[cell]}"></div>`;
-            } else {
-                divRow += `<div class="cell player">
+  let map = document.querySelector(".map");
+  map.innerHTML = '';
+  let row_index = 0;
+  for (let row of data) {
+    let col_index = 0;
+    let divRow = '';
+    for (let cell of row) {
+      if (cell.length != 2) {
+        divRow += `<div class="cell ${symbols[cell]}"></div>`;
+      } else {
+        divRow += `<div class="cell player">
                                <div data-player="${cell[0]}" data-direction="${cell[1]}" data-row="${row_index}" data-col="${col_index}"></div>
                            </div>`;
-            }
-            col_index++;
-        }
-        map.insertAdjacentHTML('beforeend',`<div class="row">
+      }
+      col_index++;
+    }
+    map.insertAdjacentHTML('beforeend', `<div class="row">
                                                             ${divRow}
                                                         </div>`)
-        row_index++;
-    }
+    row_index++;
+  }
 }
 
 let symbols = {
-    'X': 'wall',
-    'E': 'empty',
-    'B': 'box',
+  'X': 'wall',
+  'E': 'empty',
+  'B': 'box',
+  'C': 'count_upgrade',
+  'S': 'size_upgrade',
 }
