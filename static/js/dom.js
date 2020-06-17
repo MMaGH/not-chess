01ed617ccca09375300b34cf38df10d22e0ce3da
Adam Sallai <yadam1998@gmail.com>
2020-06-17 21:59:41 +0200

    visualized explosions

--- static/js/dom.js
+++ static/js/dom.js
@@ -71,6 +71,9 @@ function showMap(data) {
                 else if (!isNaN(char) && char[0] === '0') {
                     divRow += `<div class="bomb" data-playernumber="${char[1]}"></div>`;
                 }
+                else {
+                    divRow += `<div class="fire" data-playernumber="${char[1]}" data-direction="${char[0]}"></div>`;
+                }
             }
             divRow += '</div>';
             col_index++;
@@ -82,6 +85,7 @@ function showMap(data) {
     }
     showCharacters();
     showBombs();
+    showExplosion();
 }
 
 function showCharacters() {
@@ -99,6 +103,24 @@ function showBombs() {
         bomb.style.background = `url('/static/assets/player${bomb.dataset.playernumber}_bomb.png') no-repeat`;
         bomb.style.backgroundSize = 'cover';
     }
+}
+
+
+function showExplosion() {
+    let fires = document.querySelectorAll('.fire');
+    for (let fire of fires) {
+        fire.style.background = `url('/static/assets/player${fire.dataset.playernumber}_bomb_${explosion[fire.dataset.direction][0]}.png') no-repeat`;
+        fire.style.backgroundSize = 'cover';
+        fire.style.transform = `rotate(${explosion[fire.dataset.direction][1]}deg)`
+    }
+}
+
+let explosion = {
+    'M': ['middle', 0],
+    'D': ['edge', 270],
+    'U': ['edge', 90],
+    'R': ['edge', 180],
+    'L': ['edge', 0],
 }
 
 
